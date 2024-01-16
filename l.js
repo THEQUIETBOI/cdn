d49c1d1dfc183456a01db913db83954a02267f44
THEQUIETBOI <124404742+THEQUIETBOI@users.noreply.github.com>
2024-01-16 18:52:13 -0500

    Update l.js

--- l.js
+++ l.js
@@ -1,7 +1,7 @@
 <script>
     function loadImage() {
         // Replace 'YOUR_CDN_IMAGE_URL' with the actual CDN link to the image
-        var cdnImageUrl = 'https://cdn.jsdelivr.net/gh//THEQUIETBOI/cdn/download.jpg';
+        var cdnImageUrl = 'https://raw.githubusercontent.com/THEQUIETBOI/cdn/main/download.jpg';
         
         // Get the image container element
         var imageContainer = document.getElementById('imageContainer');
